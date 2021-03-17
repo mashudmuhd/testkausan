1ed50f4e52ddda7eb5f9b73bf3201030e08d2b59
Mashhood <mashhooduae@gmail.com>
2021-03-17 13:43:14 -0700

    commit

---
+++ script.js
@@ -0,0 +1,103 @@
+class CitiesSlider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.IMAGE_PARTS = 4;
+
+    this.changeTO = null;
+    this.AUTOCHANGE_TIME = 4000;
+
+    this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.changeTO);
+  }
+
+  componentDidMount() {
+    this.runAutochangeTO();
+    setTimeout(() => {
+      this.setState({ activeSlide: 0, sliderReady: true });
+    }, 0);
+  }
+
+  runAutochangeTO() {
+    this.changeTO = setTimeout(() => {
+      this.changeSlides(1);
+      this.runAutochangeTO();
+    }, this.AUTOCHANGE_TIME);
+  }
+
+  changeSlides(change) {
+    window.clearTimeout(this.changeTO);
+    const { length } = this.props.slides;
+    const prevSlide = this.state.activeSlide;
+    let activeSlide = prevSlide + change;
+    if (activeSlide < 0) activeSlide = length - 1;
+    if (activeSlide >= length) activeSlide = 0;
+    this.setState({ activeSlide, prevSlide });
+  }
+
+  render() {
+    const { activeSlide, prevSlide, sliderReady } = this.state;
+    return (
+      React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) },
+      React.createElement("p", { className: "slider__top-heading" }, ""),
+      React.createElement("div", { className: "slider__slides" },
+      this.props.slides.map((slide, index) =>
+      React.createElement("div", {
+        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+        key: slide.city },
+
+      React.createElement("div", { className: "slider__slide-content" },
+      React.createElement("h3", { className: "slider__slide-subheading" }, slide.country || slide.city),
+      React.createElement("h2", { className: "slider__slide-heading" },
+      slide.city.split('').map(l => React.createElement("span", null, l))),
+
+      React.createElement("p", { className: "slider__slide-readmore" }, " ")),
+
+      React.createElement("div", { className: "slider__slide-parts" },
+      [...Array(this.IMAGE_PARTS).fill()].map((x, i) =>
+      React.createElement("div", { className: "slider__slide-part", key: i },
+      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))),
+
+
+
+
+
+
+      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }),
+      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })));
+
+
+  }}
+
+
+const slides = [
+{
+  city: 'We Are Kauson group',
+  country: '',
+  img: 'https://images.unsplash.com/photo-1507679799987-c73779587ccf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80' },
+
+{
+  city: 'business across world wide',
+  img: 'https://images.unsplash.com/photo-1523875194681-bedd468c58bf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80' },
+
+{
+  city: '',
+  country: 'business across world wide',
+  img: 'https://images.unsplash.com/photo-1488998527040-85054a85150e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' },
+
+{
+  city: 'We are kauson group',
+  country: '',
+  img: 'https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80' },
+
+{
+  city: '',
+  country: '',
+  img: 'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80' }];
+
+
+
+ReactDOM.render(React.createElement(CitiesSlider, { slides: slides }), document.querySelector('#app'));
